a07b83c79ed044f78e774de642aa1cec494065ab
Oskar Dyka <oskar@teksusik.pl>
2023-01-02 17:39:42 +0100

    Migrate from NMS to built-in Paper PlayerProfile (#4)
    * Remove nms modules and dependencies
    
    * Remove reflections for updating player, remove references to NMS, use builtin PlayerProfile class

--- nms/V1_16/build.gradle.kts
+++
@@ -1,4 +0,0 @@
-dependencies {
-    compileOnly(project(":nms:api"))
-    compileOnly("org.spigotmc:spigot:1.16.5-R0.1-SNAPSHOT")
-}
--- plugin/src/main/java/pl/teksusik/customskins/skin/SkinService.java
+++ plugin/src/main/java/pl/teksusik/customskins/skin/SkinService.java
@@ -1,85 +1,27 @@
 package pl.teksusik.customskins.skin;
 
+import com.destroystokyo.paper.profile.PlayerProfile;
+import com.destroystokyo.paper.profile.ProfileProperty;
 import com.google.inject.Inject;
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
-import com.mojang.authlib.properties.PropertyMap;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.mineskin.MineskinClient;
 import org.mineskin.SkinOptions;
-import pl.teksusik.customskins.CustomSkinsPlugin;
-import pl.teksusik.customskins.nms.NmsAccessor;
 import pl.teksusik.customskins.storage.Storage;
-import pl.teksusik.customskins.util.ReflectionHelper;
 
-import javax.management.ReflectionException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.concurrent.CompletableFuture;
 
 public class SkinService {
     @Inject
-    private CustomSkinsPlugin plugin;
-    @Inject
     private Storage storage;
-    @Inject
-    private NmsAccessor nmsAccessor;
     @Inject
     private MineskinClient mineskinClient;
 
-    private static final Method refreshPlayerMethod;
-    private static final Method getHandleMethod;
-    private static Method healthUpdateMethod;
-
-    static {
-        try {
-            refreshPlayerMethod = ReflectionHelper.getBukkitClass("entity.CraftPlayer").getDeclaredMethod("refreshPlayer");
-            refreshPlayerMethod.setAccessible(true);
-            getHandleMethod = ReflectionHelper.getBukkitClass("entity.CraftPlayer").getDeclaredMethod("getHandle");
-            getHandleMethod.setAccessible(true);
-
-            // XP won't get updated on unsupported Paper builds
-            try {
-                healthUpdateMethod = ReflectionHelper.getBukkitClass("entity.CraftPlayer").getDeclaredMethod("triggerHealthUpdate");
-                healthUpdateMethod.setAccessible(true);
-            } catch (NoSuchMethodException ignored) {
-            }
-
-        } catch (Exception exception) {
-            throw new ExceptionInInitializerError(exception);
-        }
-    }
-
-    private void updateSkin(Player player) {
-        try {
-            refreshPlayerMethod.invoke(player);
-
-            if (healthUpdateMethod != null) {
-                healthUpdateMethod.invoke(player);
-            } else {
-                ReflectionHelper.invokeMethod(getHandleMethod.invoke(player), "triggerHealthUpdate");
-            }
-        } catch (IllegalAccessException | InvocationTargetException | ReflectionException ignored) {
-        }
-    }
 
     public void setSkin(Player player, CustomSkin customSkin) {
-        GameProfile gameProfile = this.nmsAccessor.getGameProfile(player);
-        PropertyMap propertyMap = gameProfile.getProperties();
-        if (propertyMap != null)
-            propertyMap.clear();
-        propertyMap.put("textures", new Property("textures",
-            customSkin.getTexture(),
-            customSkin.getSignature()));
-        Bukkit.getScheduler().runTask(this.plugin, () -> Bukkit.getOnlinePlayers()
-            .stream()
-            .filter(players -> !player.equals(players))
-            .forEach(players -> {
-                players.hidePlayer(this.plugin, player);
-                players.showPlayer(this.plugin, player);
-            }));
-        this.updateSkin(player);
+        PlayerProfile profile = player.getPlayerProfile();
+        ProfileProperty textures = new ProfileProperty("textures", customSkin.getTexture(), customSkin.getSignature());
+        profile.setProperty(textures);
+        player.setPlayerProfile(profile);
     }
 
     public CompletableFuture<Void> uploadSkin(Player player, String name, String url, SkinOptions skinOptions) {
